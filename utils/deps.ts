8ce6d7222f9bae7468ba383795dd635a3b01bd06
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-05-31 12:59:44 -0700

    chore: update deps

--- utils/deps.ts
+++ utils/deps.ts
@@ -1,5 +1,5 @@
-export type { Account, Block, ReadOnlyFn, TxReceipt } from "https://deno.land/x/clarinet@v1.7.1/index.ts";
+export type { Account, Block, ReadOnlyFn, TxReceipt } from "https://deno.land/x/clarinet@v1.8.0/index.ts";
 
-export { Clarinet, Chain, Tx, types } from "https://deno.land/x/clarinet@v1.7.1/index.ts";
+export { Clarinet, Chain, Tx, types } from "https://deno.land/x/clarinet@v1.8.0/index.ts";
 
 export { assertEquals, assert } from "https://deno.land/std@0.201.0/testing/asserts.ts";
