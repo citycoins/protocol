db24f99ca193689b47264e80631c9364409771f5
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-06-12 13:26:58 -0700

    fix: add missing test for trying to enable extension twice

--- tests/extensions/ccd012-redemption-nyc.test.ts
+++ tests/extensions/ccd012-redemption-nyc.test.ts
@@ -71,7 +71,70 @@ Clarinet.test({
 Clarinet.test({
   name: "ccd012-redemption-nyc: initialize-redemption() fails with ERR_ALREADY_ENABLED if called more than once",
   async fn(chain: Chain, accounts: Map<string, Account>) {
-    0;
+    // arrange
+    const sender = accounts.get("deployer")!;
+    const user1 = accounts.get("wallet_1")!;
+    const user2 = accounts.get("wallet_2")!;
+    const user3 = accounts.get("wallet_3")!;
+    const ccd007CityStacking = new CCD007CityStacking(chain, sender, "ccd007-citycoin-stacking");
+    const ccip022TreasuryRedemptionNyc = new CCIP022TreasuryRedemptionNYC(chain, sender);
+
+    // set stacking parameters
+    const amountStacked = 500;
+    const lockPeriod = 10;
+
+    // progress the chain to avoid underflow in
+    // stacking reward cycle calculation
+    chain.mineEmptyBlockUntil(CCD007CityStacking.FIRST_STACKING_BLOCK);
+
+    // initialize contracts
+    constructAndPassProposal(chain, accounts, PROPOSALS.TEST_CCIP022_TREASURY_REDEMPTION_NYC_001);
+
+    // mint and move funds
+    passProposal(chain, accounts, PROPOSALS.TEST_CCIP022_TREASURY_REDEMPTION_NYC_002);
+    passProposal(chain, accounts, PROPOSALS.TEST_CCIP022_TREASURY_REDEMPTION_NYC_003);
+
+    // stack first cycle u1, last cycle u10
+    const stackingBlock = chain.mineBlock([ccd007CityStacking.stack(user1, nyc.cityName, amountStacked, lockPeriod), ccd007CityStacking.stack(user2, nyc.cityName, amountStacked, lockPeriod), ccd007CityStacking.stack(user3, nyc.cityName, amountStacked, lockPeriod)]);
+    // for length of mineBlock array, expectOk and expectBool(true)
+    for (let i = 0; i < stackingBlock.receipts.length; i++) {
+      stackingBlock.receipts[i].result.expectOk().expectBool(true);
+    }
+
+    // progress the chain to cycle 5
+    // votes are counted in cycles 2-3
+    // past payouts tested for cycles 1-4
+    chain.mineEmptyBlockUntil(CCD007CityStacking.REWARD_CYCLE_LENGTH * 6 + 10);
+    ccd007CityStacking.getCurrentRewardCycle().result.expectUint(5);
+
+    // execute two yes votes, one no vote
+    const votingBlock = chain.mineBlock([ccip022TreasuryRedemptionNyc.voteOnProposal(user1, true), ccip022TreasuryRedemptionNyc.voteOnProposal(user2, true), ccip022TreasuryRedemptionNyc.voteOnProposal(user3, false)]);
+    for (let i = 0; i < votingBlock.receipts.length; i++) {
+      votingBlock.receipts[i].result.expectOk().expectBool(true);
+    }
+
+    const executeBlock = passProposal(chain, accounts, PROPOSALS.CCIP_022);
+
+    assertEquals(executeBlock.receipts.length, 3);
+    for (let i = 0; i < executeBlock.receipts.length; i++) {
+      executeBlock.receipts[i].result.expectOk().expectUint(i + 1);
+    }
+
+    const expectedEvent = `{notification: "intialize-contract", payload: {blockHeight: u12611, contractBalance: u15000000000000, currentContractBalance: u15000000000000, redemptionRatio: u292282, redemptionsEnabled: true, totalRedeemed: u0, totalSupply: u5132020000000000}}`;
+    // redemption ratio obtained through console logging below
+    // console.log(expectedEvent);
+    // console.log(executeBlock.receipts[2].events[3].contract_event.value);
+
+    executeBlock.receipts[2].events.expectPrintEvent(EXTENSIONS.CCD012_REDEMPTION_NYC, expectedEvent);
+
+    // act
+    const secondInit = passProposal(chain, accounts, PROPOSALS.TEST_CCIP022_TREASURY_REDEMPTION_NYC_004);
+
+    // assert
+    assertEquals(secondInit.receipts.length, 3);
+    secondInit.receipts[0].result.expectOk().expectUint(1);
+    secondInit.receipts[1].result.expectOk().expectUint(2);
+    secondInit.receipts[2].result.expectErr().expectUint(CCD012RedemptionNyc.ErrCode.ERR_ALREADY_ENABLED);
   },
 });
 
@@ -131,7 +194,6 @@ Clarinet.test({
 
     const expectedEvent = `{notification: "intialize-contract", payload: {blockHeight: u12611, contractBalance: u15000000000000, currentContractBalance: u15000000000000, redemptionRatio: u292282, redemptionsEnabled: true, totalRedeemed: u0, totalSupply: u5132020000000000}}`;
     // redemption ratio obtained through console logging below
-    // verified by the values: 15000000000000 / 32020000000000 = 0.46845721
     // console.log(expectedEvent);
     // console.log(executeBlock.receipts[2].events[3].contract_event.value);
 
